d624d7593a1cc2ca4d1a9c662b5b32cf9537ac8b
Cody R Fitzgerald <codypsa@MacBook-Pro.local>
2014-12-21 15:31:58 -0500

    Initial Commit

---
+++ AliveTests/AliveTests.swift
@@ -0,0 +1,36 @@
+//
+//  AliveTests.swift
+//  AliveTests
+//
+//  Created by Cody R Fitzgerald on 12/21/14.
+//  Copyright (c) 2014 Cody R Fitzgerald. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class AliveTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
